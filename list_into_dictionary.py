c28d1e8dc49920b10cfdad213d6db1b6fb604bca
Shubhansh Programmer <shubhansh7777@gmail.com>
2021-05-31 21:47:37 +0530

    List into dictionary

--- list_into_dictionary.py
+++ list_into_dictionary.py
@@ -1,4 +1,4 @@
-print("hello world")
+'''
 animal={
     "lion": ["Carnivores","danger"],
     "cow": ["Herbivores","innocent"],
@@ -13,4 +13,18 @@ if a in animal.keys():
     animal[a].append(b)
     print(animal[a])
 else:
-    print('Animal name not available')
+    print('Animal name not available')'''
+
+
+
+animal={
+    "water_animal":["fish","shark","crocodile"],
+    "land_animal":["dinasour","lion","elephant"],
+    "sky_animal":["bird","peacock","crow"],
+    "reptile" : {
+        'land_animal' : ['lizard','monitor'],
+        'water_animal' :  'anaconda' ,
+    }
+}
+animal['reptile']['land_animal'][ 1]='dragon'
+print(animal)
---
+++ split.py
@@ -0,0 +1,5 @@
+#data = [1,3,None,4,5,None,7,8,None,10,11,None]
+
+d = [1,None,3]
+d.clear(0:2)
+print (d)
